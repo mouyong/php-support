5c0e0576f3c774df48fa1429e5329b27717ddb70
mouyong <mouyong@users.noreply.github.com>
2022-04-25 12:52:39 +0800

    Update ResponseTrait.php

--- src/Traits/ResponseTrait.php
+++ src/Traits/ResponseTrait.php
@@ -49,7 +49,7 @@ trait ResponseTrait
         $res = compact('err_code', 'err_msg', 'data') + array_filter(compact('meta'));
 
         return \response(
-            \json_encode($res, \JSON_UNESCAPED_SLASHES|\JSON_UNESCAPED_UNICODE),
+            \json_encode($res, \JSON_UNESCAPED_SLASHES|\JSON_UNESCAPED_UNICODE|\JSON_PRETTY_PRINT),
             Response::HTTP_OK,
             array_merge([
                 'Content-Type' => 'application/json',
