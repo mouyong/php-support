f03072aac314b7831ba4fbd1a752b74d5c2ce067
mouyong <mouyong@users.noreply.github.com>
2022-07-11 20:23:43 +0800

    Update Arrayable.php

--- src/Traits/Arrayable.php
+++ src/Traits/Arrayable.php
@@ -10,7 +10,7 @@ trait Arrayable
 
     public static function makeAttribute(array $attributes = [])
     {
-        $instance = new class implements \ArrayAccess
+        $instance = new class implements \ArrayAccess, \IteratorAggregate
         {
             use Arrayable;
         };
@@ -70,5 +70,10 @@ trait Arrayable
     public function toArray()
     {
         return $this->getAttributes();
+    }
+
+    public function getIterator(): \ArrayIterator
+    {
+        return new \ArrayIterator($this->toArray());
     }
 }
