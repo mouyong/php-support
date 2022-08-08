03b0a23ee71f5dbbd4e15427ebd5837ebbfaba3a
mouyong <mouyong@users.noreply.github.com>
2022-08-08 12:57:54 +0800

    Create ReplaceTrait.php

---
+++ src/Traits/ReplaceTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ZhenMu\Support\Traits;
+
+trait ReplaceTrait
+{
+    public function getReplaceKeys($content)
+    {
+        preg_match_all('/(\$[^\s.]*?\$)/', $content, $matches);
+
+        $keys = $matches[1] ?? [];
+
+        return $keys;
+    }
+
+    public function getReplacesByKeys(array $keys)
+    {
+        $replaces = [];
+        foreach ($keys as $key) {            
+            $currentReplacement = str_replace('$', '', $key);
+
+            $currentReplacementLower = Str::of($currentReplacement)->lower()->toString();
+            $method = sprintf("get%sReplacement", Str::studly($currentReplacementLower));
+
+            if (method_exists($this, $method)) {
+                $replaces[$currentReplacement] = $this->$method();
+            } else {
+                \info($currentReplacement . " does match any replace content");
+                $replaces[$currentReplacement] = null;
+            }
+        }
+
+        return $replaces;
+    }
+}
