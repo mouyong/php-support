43bacb82906d80dfc2210ee544df5fc4ea2b1e13
mouyong <mouyong@users.noreply.github.com>
2022-09-22 16:51:46 +0800

    Update Tree.php

--- src/Utils/Tree.php
+++ src/Utils/Tree.php
@@ -12,29 +12,32 @@ class Tree
      * @return null|array
      * #
      */
-    public static function toTree($data = [], $primary = 'id', $parent = 'parent_id', $children = 'children')
+    public static function toTree($data = [], $primaryId = 'id', $parentId = 'parent_id', $children = 'children')
     {
         // data is empty
         if (count($data) === 0) {
             return null;
-        }
+    }
 
         // parameter missing
-        if (!array_key_exists($primary, head($data)) || !array_key_exists($parent, head($data))){
+        if (!array_key_exists($primaryId, head($data)) || !array_key_exists($parentId, head($data))){
             return null;
         }
 
+        // map
         $items = array();
         foreach ($data as $v) {
-            $items[@$v[$primary]] = $v;
+            $v[$children] = [];
+            $items[@$v[$primaryId]] = $v;
         }
 
+        // tree
         $tree = array();
-        foreach ($items as $item) {
-            if (isset($items[$item[$parent]])) {
-                $items[$item[$parent]][$children][] = &$items[$item[$primary]];
+        foreach ($items as &$item) {
+            if (array_key_exists($item[$parentId], $items)) {
+                $items[$item[$parentId]][$children][] = &$items[$item[$primaryId]];
             } else {
-                $tree[] = &$items[$item[$primary]];
+                $tree[] = &$items[$item[$primaryId]];
             }
         }
 
