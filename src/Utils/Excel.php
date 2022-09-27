64526126a549ceaf9dd53e4da39e8caf24022405
mouyong <mouyong@users.noreply.github.com>
2022-09-27 11:09:15 +0800

    Update Excel.php

--- src/Utils/Excel.php
+++ src/Utils/Excel.php
@@ -4,12 +4,26 @@ namespace ZhenMu\Support\Utils;
 
 class Excel
 {
-    public static function datetime($datetime = null, $format = 'Y-m-d H:i:s')
+    public static function datetimeFromCell($datetime = null, $format = 'Y-m-d H:i:s')
     {
         if (!$datetime) {
             return null;
         }
 
-        return \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($datetime)->format($format);
+        return \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($datetime)?->format($format);
+    }
+    
+    public static function cellToString($format = '="%s"', ...$value)
+    {
+        if (!str_starts_with($format, '=')) {
+            $value = [$format];
+            $format = '="%s"';
+        }
+
+        if (!$value) {
+            return null;
+        }
+
+        return sprintf($format, ...$value);
     }
 }
