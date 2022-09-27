fe24117ce2d1b624b4bf0921260d627ed894f2d0
mouyong <mouyong@users.noreply.github.com>
2022-09-27 16:39:03 +0800

    Update Excel.php

--- src/Utils/Excel.php
+++ src/Utils/Excel.php
@@ -2,14 +2,26 @@
 
 namespace ZhenMu\Support\Utils;
 
+use Carbon\Carbon;
+
 class Excel
 {
-    public static function datetimeFromCell($datetime = null, $format = 'Y-m-d H:i:s')
+    public static function datetimeFromCell($datetime = null, $format = 'Y-m-d H:i:s', $soruceFormat = 'Y-m-d H:i:s')
     {
         if (!$datetime) {
             return null;
         }
 
+        if (is_string($datetime)) {
+            $datetime = match(true) {
+                default => null,
+                str_contains($datetime, '-') && str_contains($datetime, ':') => Carbon::createFromFormat($soruceFormat, $datetime)->format($format),
+                str_contains($datetime, '/') && !str_contains($datetime, ':') => Carbon::createFromDate($datetime)->format($format),
+            };
+
+            return $datetime;
+        }
+        
         return \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($datetime)?->format($format);
     }
     
