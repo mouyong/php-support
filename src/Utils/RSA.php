c8552be2c85f310bb0ff22615f5fe7ba4af301fa
mouyong <mouyong@users.noreply.github.com>
2023-02-26 02:06:13 +0800

    Update RSA.php

--- src/Utils/RSA.php
+++ src/Utils/RSA.php
@@ -21,4 +21,57 @@ class RSA
             'privateKey' => $privateKey,
         ];
     }
+
+    public static function normalPublicKey($publicKey)
+    {
+        $fKey = "-----BEGIN PUBLIC KEY-----\n";
+        $len = strlen($publicKey);
+        for($i = 0; $i < $len; ) {
+            $fKey = $fKey . substr($publicKey, $i, 64) . "\n";
+            $i += 64;
+        }
+        $fKey .= "-----END PUBLIC KEY-----";
+        return $fKey;
+    }
+
+    public static function normalPrivateKey($privateKey)
+    {
+        $fKey = "-----BEGIN PRIVATE KEY-----\n";
+        $len = strlen($privateKey);
+        for($i = 0; $i < $len; ) {
+            $fKey = $fKey . substr($privateKey, $i, 64) . "\n";
+            $i += 64;
+        }
+        $fKey .= "-----END PRIVATE KEY-----";
+        return $fKey;
+    }
+
+    public static function encrypt($data, $privateKey)
+    {
+        // $privateKey = RSA::normalPrivateKey($privateKey); // 格式化私钥为标准的 private key
+
+        $res = openssl_private_encrypt($data, $encrypted, $privateKey);
+
+        if(!$res) {
+            return false;
+        }
+
+        return Str::stringToHex($encrypted);
+    }
+
+    public function decrypt($data, $publicKey)
+    {
+        // $publicKey = RSA::normalPublicKey($publicKey); // 格式化公钥为标准的 public key
+
+        // $openssl_pub = openssl_pkey_get_public($publicKey); // 不知道作用，未使用
+
+        // 验签
+        $resArr = openssl_public_decrypt(Str::hexToString($data), $decrypted, $publicKey);
+
+        if(!$resArr){
+            return false;
+        }
+
+        return $decrypted;
+    }
 }
